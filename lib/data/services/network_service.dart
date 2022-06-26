aaf93a1acb7293521a3d943ea0f2f0d7a73afb5d
Georgey Gabaidze <gabaidzegeorgij@gmail.com>
2022-06-26 21:12:47 +0300

    implemented getting main breed list

--- lib/data/services/network_service.dart
+++ lib/data/services/network_service.dart
@@ -1,10 +1,20 @@
+import 'dart:convert';
+
 import 'package:http/http.dart';
 
 class NetworkService {
   void fetchDogs() async {
     var url = Uri.parse('https://dog.ceo/api/breeds/list/all');
     var response = await get(url);
-    print('Response status: ${response.statusCode}');
-    print('Response body: ${response.body}');
+    var listDogs = <String>[];
+    Map<String, dynamic> responseMap = json.decode(response.body);
+    var messageMap = Map<String, dynamic>.from(responseMap['message']);
+
+    var sortedDogKeys = messageMap.keys.toList();
+
+    print(sortedDogKeys);
+
+    //print('Response status: ${response.statusCode}');
+    // print('Response body: ${response.body}');
   }
 }
