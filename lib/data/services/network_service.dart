b820d76008ed799771b876a72a2e726a5d720f49
Georgey Gabaidze <gabaidzegeorgij@gmail.com>
2022-07-18 15:43:53 +0300

    refactored code
    Took 9 seconds

--- lib/data/services/network_service.dart
+++ lib/data/services/network_service.dart
@@ -3,25 +3,35 @@ import 'package:array_names/data/models/breed.dart';
 import 'package:http/http.dart';
 
 class NetworkService {
+  static const _baseUrl = "https://dog.ceo";
+  static const _apiUrl = "$_baseUrl/api";
+
+  static const _dogListEndpointUrl = '$_apiUrl/breeds/list/all';
+
+  static const _messageKey = 'message';
+
+  String _breedPhotoEndpointUrl(Breed breed) =>
+      '$_apiUrl/breed/${breed.breed}/images/random/10';
+
+  String _breedWithSubBreedEndpointUrl(Breed breed) =>
+      '$_apiUrl/breed/${breed.breed}/${breed.subBreed}/images/random/10';
+
   Future<Map<String, dynamic>> fetchDogs() async {
-    final url = Uri.parse('https://dog.ceo/api/breeds/list/all');
+    final url = Uri.parse(_dogListEndpointUrl);
     final response = await get(url);
     Map<String, dynamic> responseMap = json.decode(response.body);
-    final messageMap = Map<String, dynamic>.from(responseMap['message']);
+    final messageMap = Map<String, dynamic>.from(responseMap[_messageKey]);
     return messageMap;
   }
 
   Future<List<String>> fetchImages(Breed breed) async {
-    var checkSubBreed = breed.subBreed != null;
-    final url = checkSubBreed
-        ? Uri.parse(
-            'https://dog.ceo/api/breed/${breed.breed}/${breed.subBreed}/images/random/10')
-        : Uri.parse(
-            'https://dog.ceo/api/breed/${breed.breed}/images/random/10');
-
+    final hasSubBreed = breed.subBreed != null;
+    final url = Uri.parse(hasSubBreed
+        ? _breedWithSubBreedEndpointUrl(breed)
+        : _breedPhotoEndpointUrl(breed));
     final response = await get(url);
     Map<String, dynamic> responseMapImages = json.decode(response.body);
-    final messageImages = List<String>.from(responseMapImages['message']);
+    final messageImages = List<String>.from(responseMapImages[_messageKey]);
     return messageImages;
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:array_names/dogs_app.dart';
 import 'package:flutter/material.dart';
 
-// todo фотографии должны грузиться в зависимости от породы
 // todo не должно быть ошибки, если меньше 10 фотографий в пароде
 void main() {
   runApp(const DogsApp());
