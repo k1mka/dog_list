e83647203b6c0c420b8300fceca53a8c3c25e5f7
Georgey Gabaidze <gabaidzegeorgij@gmail.com>
2022-07-29 15:14:49 +0300

    implemented dogs to ui with revirpod provider
    Took 1 hour 19 minutes

--- lib/business_logic/dog_list_provider.dart
+++ lib/business_logic/dog_list_provider.dart
@@ -3,10 +3,11 @@ import 'package:array_names/data/repositories/repository.dart';
 import 'package:array_names/service_locator.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-final getDogListProvider = StateNotifierProvider(
+final getDogListProvider = StateNotifierProvider<DogListProvider, List<Breed>>(
   (ref) {
     final repository = ref.read(repositoryProvider);
-    return DogListProvider(repository);
+    final provider = DogListProvider(repository);
+    return provider;
   },
 );
 
@@ -14,8 +15,8 @@ class DogListProvider extends StateNotifier<List<Breed>> {
   final Repository repo;
 
   DogListProvider(this.repo) : super([]);
-  getDogs() async {
+  void getDogs() async {
     final dogList = await repo.fetchDogs();
-    return dogList;
+    state = dogList;
   }
 }
