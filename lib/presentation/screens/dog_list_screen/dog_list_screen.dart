41673e88c886b3249480fb9e93fc8db97f0c70d0
Georgey Gabaidze <gabaidzegeorgij@gmail.com>
2022-08-05 15:13:58 +0300

    wrapped DogListLayout with BlocProvider and get it
    Took 2 minutes

--- lib/presentation/screens/dog_list_screen/dog_list_screen.dart
+++ lib/presentation/screens/dog_list_screen/dog_list_screen.dart
@@ -1,11 +1,19 @@
+import 'package:array_names/data/repositories/repository.dart';
+import 'package:array_names/presentation/get_it.dart';
 import 'package:array_names/presentation/screens/dog_list_screen/dog_list_layout.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../../business_logic/dog_list_cubit.dart';
 
 class DogsListScreen extends StatelessWidget {
   const DogsListScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const DogListLayout();
+    return BlocProvider(
+      create: (_) => DogListCubit(getIt<Repository>()),
+      child: const DogListLayout(),
+    );
   }
 }
