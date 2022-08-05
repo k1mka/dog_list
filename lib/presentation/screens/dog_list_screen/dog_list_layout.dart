6ff96fc2bbeb06d64560d0e453014d8ddf36d390
Georgey Gabaidze <gabaidzegeorgij@gmail.com>
2022-08-05 15:11:31 +0300

    implemented fetch dogs to ua from BlocProvider
    Took 2 minutes

--- lib/presentation/screens/dog_list_screen/dog_list_layout.dart
+++ lib/presentation/screens/dog_list_screen/dog_list_layout.dart
@@ -1,38 +1,37 @@
-import 'package:array_names/business_logic/dog_list_provider.dart';
-
+import 'package:array_names/business_logic/dog_list_cubit.dart';
+import 'package:array_names/data/models/breed.dart';
 import 'package:array_names/presentation/widgets/breed_widget.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-class DogListLayout extends ConsumerStatefulWidget {
+class DogListLayout extends StatefulWidget {
   const DogListLayout({Key? key}) : super(key: key);
 
   @override
-  ConsumerState<ConsumerStatefulWidget> createState() => DogListLayoutState();
+  State<DogListLayout> createState() => _DogListLayoutState();
 }
 
-class DogListLayoutState extends ConsumerState<ConsumerStatefulWidget> {
+class _DogListLayoutState extends State<DogListLayout> {
   @override
-  void initState() {
-    ref.read(getDogListProvider.notifier).getDogs();
-    super.initState();
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.black87,
+          title: const Text('DogsListScreen'),
+        ),
+        body: Center(
+          child: BlocBuilder<DogListCubit, List<Breed>>(
+              builder: (context, breedList) => ListView.separated(
+                    itemCount: breedList.length,
+                    itemBuilder: (_, index) => BreedWidget(breedList[index]),
+                    separatorBuilder: (_, __) => const Divider(thickness: 4),
+                  )),
+        ));
   }
 
   @override
-  Widget build(BuildContext context) {
-    final breedList = ref.watch(getDogListProvider);
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.black87,
-        title: const Text('DogsListScreen'),
-      ),
-      body: Center(
-        child: ListView.separated(
-          itemCount: breedList.length,
-          itemBuilder: (_, index) => BreedWidget(breedList[index]),
-          separatorBuilder: (_, __) => const Divider(thickness: 4),
-        ),
-      ),
-    );
+  void initState() {
+    super.initState();
+    context.read<DogListCubit>().cubitFetchDogs();
   }
 }
