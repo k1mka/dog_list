81296a3755d183f27879d7e93a4a2646f291a47e
Georgey Gabaidze <gabaidzegeorgij@gmail.com>
2022-08-10 21:05:18 +0300

    refactored cubit to bloc
    Took 1 hour 2 minutes

--- lib/business_logic/dog_list_cubit.dart
+++ lib/business_logic/dog_list_cubit.dart
@@ -1,18 +1,19 @@
 import 'package:array_names/business_logic/cubit_states/dog_list_state.dart';
+import 'package:array_names/business_logic/cubit_states/events.dart';
 import 'package:array_names/data/repositories/repository.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-class DogListCubit extends Cubit<DogListState> {
+class DogListBloc extends Bloc<Events, DogListState> {
   Repository repo;
-  DogListCubit(this.repo) : super(InitialState());
-
-  void cubitFetchDogs() async {
-    emit(LoadingDogs()); // заэмитить
-    try {
-      final breeds = await repo.fetchDogs();
-      emit(LoadedDogs(breeds));
-    } catch (e) {
-      emit(ErrorDogs(e));
-    }
+  DogListBloc(this.repo) : super(InitialState()) {
+    on<LoadingDogsEvent>((event, emit) async {
+      emit(LoadingDogs());
+      try {
+        final breeds = await repo.fetchDogs();
+        emit(LoadedDogs(breeds));
+      } catch (e) {
+        emit(ErrorDogs(e));
+      }
+    });
   }
 }
