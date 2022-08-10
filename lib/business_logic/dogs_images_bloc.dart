4218fd3a247bcd0abab36da50b7749e225dabdd9
Georgey Gabaidze <gabaidzegeorgij@gmail.com>
2022-08-10 21:11:51 +0300

    refactored cubit to bloc
    Took 3 minutes

---
+++ lib/business_logic/dogs_images_bloc.dart
@@ -0,0 +1,22 @@
+import 'package:array_names/business_logic/cubit_states/events.dart';
+import 'package:array_names/business_logic/cubit_states/images_state.dart';
+import 'package:array_names/data/models/breed.dart';
+import 'package:array_names/data/repositories/repository.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class DogsImagesBloc extends Bloc<Events, ImagesState> {
+  final Repository repo;
+  final Breed breed;
+
+  DogsImagesBloc(this.repo, this.breed) : super(InitialState()) {
+    on<LoadingImagesEvent>((event, emit) async {
+      emit(LoadingDogsImages());
+      try {
+        final getImages = await repo.getAlbum(breed);
+        emit(LoadedImages(getImages));
+      } catch (e) {
+        emit(ImagesError(e));
+      }
+    });
+  }
+}
