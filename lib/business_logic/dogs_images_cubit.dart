6d1d88bb0d3ed6f7e9418a624e3ea3512fffc91e
Georgey Gabaidze <gabaidzegeorgij@gmail.com>
2022-08-10 16:11:57 +0300

    rewrote DogsImagesCubit with states
    Took 43 seconds

--- lib/business_logic/dogs_images_cubit.dart
+++ lib/business_logic/dogs_images_cubit.dart
@@ -1,11 +1,20 @@
+import 'package:array_names/business_logic/cubit_states/images_state.dart';
 import 'package:array_names/data/models/breed.dart';
 import 'package:array_names/data/repositories/repository.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-class DogsImagesCubit extends Cubit<List<String>> {
+class DogsImagesCubit extends Cubit<ImagesState> {
   final Repository repo;
 
-  DogsImagesCubit(this.repo) : super([]);
+  DogsImagesCubit(this.repo) : super(InitialState());
 
-  void cubitGetImages(Breed breed) async => emit(await repo.getAlbum(breed));
+  void cubitGetImages(Breed breed) async {
+    emit(LoadingDogsImages());
+    try {
+      final getImages = await repo.getAlbum(breed);
+      emit(LoadedImages(getImages));
+    } catch (e) {
+      emit(ImagesError(e));
+    }
+  }
 }
