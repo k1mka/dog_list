84c50b811cc7a66725a72c13c29e9524cd0e84b5
Georgey Gabaidze <gabaidzegeorgij@gmail.com>
2022-08-10 13:31:21 +0300

    added and processed states
    Took 37 seconds

--- lib/presentation/screens/dog_list_screen/dog_list_layout.dart
+++ lib/presentation/screens/dog_list_screen/dog_list_layout.dart
@@ -1,6 +1,7 @@
+import 'package:array_names/business_logic/cubit_states/dog_list_state.dart';
 import 'package:array_names/business_logic/dog_list_cubit.dart';
-import 'package:array_names/data/models/breed.dart';
 import 'package:array_names/presentation/widgets/breed_widget.dart';
+import 'package:array_names/presentation/widgets/dog_list_error_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -13,25 +14,39 @@ class DogListLayout extends StatefulWidget {
 
 class _DogListLayoutState extends State<DogListLayout> {
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.black87,
-          title: const Text('DogsListScreen'),
-        ),
-        body: Center(
-          child: BlocBuilder<DogListCubit, List<Breed>>(
-              builder: (context, breedList) => ListView.separated(
-                    itemCount: breedList.length,
-                    itemBuilder: (_, index) => BreedWidget(breedList[index]),
-                    separatorBuilder: (_, __) => const Divider(thickness: 4),
-                  )),
-        ));
-  }
-
-  @override
   void initState() {
     super.initState();
     context.read<DogListCubit>().cubitFetchDogs();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black87,
+        title: const Text('DogsListScreen'),
+      ),
+      body: Center(
+        child: BlocBuilder<DogListCubit, DogListState>(
+          builder: (context, state) {
+            if (state is InitialState) {
+              return const Center(child: Text('expectation'));
+            } else if (state is ErrorDogs) {
+              return const DogListErrorWidget();
+            } else if (state is LoadingDogs) {
+              return const CircularProgressIndicator();
+            } else if (state is LoadedDogs) {
+              return ListView.separated(
+                itemCount: state.breedList.length,
+                itemBuilder: (_, index) => BreedWidget(state.breedList[index]),
+                separatorBuilder: (_, __) => const Divider(thickness: 4),
+              );
+            } else {
+              throw Exception('unprocessed state $state in DogListLayout');
+            }
+          },
+        ),
+      ),
+    );
   }
 }
