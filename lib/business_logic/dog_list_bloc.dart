f93a7ad4db1a131bc3b4f2c3636562ba4b7288e7
Georgey Gabaidze <gabaidzegeorgij@gmail.com>
2022-11-09 21:19:32 +0200

    processed sendAnalyticsEvent in bloc
    Took 2 minutes

--- lib/business_logic/dog_list_bloc.dart
+++ lib/business_logic/dog_list_bloc.dart
@@ -1,11 +1,13 @@
 import 'package:array_names/business_logic/cubit_states/dog_list_state.dart';
 import 'package:array_names/business_logic/cubit_states/dogs_event.dart';
 import 'package:array_names/data/repositories/repository.dart';
+import 'package:array_names/data/services/analytic_service.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class DogListBloc extends Bloc<DogsEvent, DogListState> {
   Repository repo;
-  DogListBloc(this.repo) : super(InitialState()) {
+  AnalyticService analyticService;
+  DogListBloc(this.repo, this.analyticService) : super(InitialState()) {
     on<LoadingDogsEvent>((event, emit) async {
       emit(LoadingDogs());
       try {
@@ -13,6 +15,14 @@ class DogListBloc extends Bloc<DogsEvent, DogListState> {
         emit(LoadedDogs(breeds));
       } catch (e) {
         emit(ErrorDogs(e));
+      }
+    });
+    on<FireBaseAnalyticEvent>((event, emit) async {
+      try {
+        await analyticService.sendAnalyticsEvent(
+            eventName: 'click_on_the_${event.breed.getFullNameToFirebase}', clickEvent: 'clickEvent');
+      } catch (e) {
+        print('error in Block');
       }
     });
   }
